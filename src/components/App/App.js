95e8f4e06ae026111690962c5c50d0a553b40bec
Ainaz Kambar <100521918+moontegrate@users.noreply.github.com>
2024-03-10 12:49:28 +0500

    Сделал шапку

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,18 +5,27 @@ import { load } from '@2gis/mapgl';
 const App = () => {
   load().then((mapglAPI) => {
     const map = new mapglAPI.Map('map-wrapper', {
-       key: '8aa9d22a-14aa-408d-bbdd-faa892eb1d05',
-        center: [37.65, 55.74],
-        zoom: 10,
+      key: '8aa9d22a-14aa-408d-bbdd-faa892eb1d05',
+      center: [71.43, 51.12],
+      zoom: 10,
+      zoomControl: 'centerRight'
     });
   });
 
   return (
     <div className="App">
       <header className="header">
+        <div className='header__bar'>
+          <div className='header__bar-logo'></div>
+        </div>
+        <div className='header__profile-btn'>
+          <img src='http://localhost:8000/media/user_photos/Profile_BW.jpg' alt='profile'/>
+        </div>
       </header>
-        <div id="map-wrapper" style={{ 'height': '100vh'}}>
-      </div>
+      <aside>
+        
+      </aside>
+      <div id="map-wrapper" style={{ 'height': '100vh'}}/>
     </div>
   );
 };
