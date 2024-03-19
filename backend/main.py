a7051c306e1723d607ee0e3f2bf46f4c69b683eb
kelliosli <excise.exe@gmail.com>
2024-03-19 18:35:33 +0500

    fixing dockerfile
    added auto updates on bd

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI, APIRouter
-
 from fastapi.middleware.cors import CORSMiddleware
 
+from backend.database import create_tables
+
 app = FastAPI()
+
+@app.on_event("startup")
+async def on_startup():
+    await create_tables()
+
+
 
 origins = [
     "http://localhost",
     "http://localhost:3000",
 ]
-
-
 
 app.add_middleware(
     CORSMiddleware,
