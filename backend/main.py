1b78e5084b0537238795e66ca867e8a1db6218b2
sasha26548996565 <68546453+sasha26548996565@users.noreply.github.com>
2024-03-20 16:01:56 +0500

    test auth

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from backend.database import create_tables
 
+from fastapi_users import fastapi_users, FastAPIUsers
+from pydantic import BaseModel, Field
+
+from fastapi import FastAPI, Request, status, Depends
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ValidationError
+from fastapi.responses import JSONResponse
+
 app = FastAPI()
 
 origins = [
@@ -17,3 +25,22 @@ app.add_middleware(
     allow_methods=["GET", "PUT", "POST", "PATCH", "DELETE"],
     allow_headers=["*"],
 )
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
