ca16c06e0fe1fabfc00add5918245cf65b6b91fc
Ainaz Kambar <100521918+moontegrate@users.noreply.github.com>
2024-03-25 21:47:28 +0500

    Map control recovered

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -7,13 +7,13 @@ import { useEffect, useState } from 'react';
 
 // Redux
 import { useDispatch, useSelector } from 'react-redux';
-import { setCurrentLocation } from './mapSlice';
+import { setCurrentLocation, incZoom, decZoom, setCenter, setZoom } from './mapSlice';
 import { setOrders } from '../App/ordersSlice';
 
 // http
 import { _host } from '../../services/http';
 
-import { YMaps, Map, Placemark, ZoomControl } from '@pbe/react-yandex-maps';
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
 // export const _key = '8aa9d22a-14aa-408d-bbdd-faa892eb1d05'
 export const _key = 'fake'
@@ -22,21 +22,54 @@ const MainMap = () => {
     const dispatch = useDispatch();
 
     const orders = useSelector((state) => state.orders.orders);
+    const zoom = useSelector((state) => state.map.zoom);
+    const center = useSelector((state) => state.map.center);
 
-    const defaultState = {
-        center: [51.12, 71.43],
-        zoom: 9,
-        controls: []
+    const getUserLocation = () => {
+        return new Promise((resolve, reject) => {
+            if ('geolocation' in navigator) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        resolve([position.coords.latitude, position.coords.longitude]);
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                );
+            } else {
+                reject(new Error('Geolocation is not supported by your browser'));
+            };
+        });
     };
 
     return (
         <div id='map-wrapper'>
             <YMaps>
-                <Map defaultState={defaultState} height='100vh' width='100vw'>
+                <Map state={{center: center, zoom: zoom}} height='100vh' width='100vw'>
                     <Placemark geometry={[55.684758, 37.738521]} />
-                    <ZoomControl options={{ position: {top: 300, right: 20}}} />
                 </Map>
             </YMaps>
+            <div className='map-controls'>
+                <div className='zoom-control'>
+                    <div className='zoom-in' onClick={() => dispatch(incZoom())}>+</div>
+                    <div className='zoom-out' onClick={() => dispatch(decZoom())}>-</div>
+                </div>
+                <div className='current-location' onClick={() => {
+                    getUserLocation()
+                    .then((coordinates) => {
+                        console.log(coordinates)
+                        dispatch(setCenter(coordinates));
+                        dispatch(setZoom(15));
+                        dispatch(setCurrentLocation(coordinates));
+                        // if (currentLocationMarker) {
+                        //     currentLocationMarker.setCoordinates(coordinates);
+                        // };
+                    })
+                    .catch((e) => console.error(e));
+                }}>
+                    <PiNavigationArrowBold />
+                </div>
+            </div>
         </div>
     );
 };
--- frontend/src/components/Map/mapSlice.js
+++ frontend/src/components/Map/mapSlice.js
@@ -1,16 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentLocation: null
+    currentLocation: null,
+    zoom: 9,
+    center: [51.12, 71.43]
 };
 
 const map = createSlice({
     name: 'map',
     initialState,
     reducers: {
-        setCurrentLocation: (state, action) => {state.currentLocation = action.payload}
+        setCurrentLocation: (state, action) => {state.currentLocation = action.payload},
+        incZoom: (state, action) => {
+            if (state.zoom < 20) {
+                state.zoom = ++state.zoom;
+            }
+        },
+        decZoom: (state, action) => {
+            if (state.zoom > 4) {
+                state.zoom = --state.zoom;
+            }
+        },
+        setCenter: (state, action) => {state.center = action.payload},
+        setZoom: (state, action) => {state.zoom = action.payload}
     }
 });
 
 export default map.reducer;
-export const { setCurrentLocation } = map.actions;
+export const { setCurrentLocation, incZoom, decZoom, setCenter, setZoom } = map.actions;
